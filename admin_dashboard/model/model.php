0a885ba22445a72ff22e8cfdcfd3bc7a42672616
Atma1 <atmasusanto08@gmail.com>
2024-06-06 13:32:49 +0800

    bug fixes

--- admin_dashboard/model/model.php
+++ admin_dashboard/model/model.php
@@ -1,5 +1,16 @@
 <?php
-include_once '../go-berkah-resto/config.php';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "restoran";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 header('Content-Type: application/json');
 
@@ -22,21 +33,30 @@ if (!in_array($route, $validRoutes)) {
 }
 
 // Fetch data from the corresponding table
-$sql = "SELECT * FROM $route";
+$sql = "SELECT id, nama, harga, img, keterangan FROM $route";
 $result = $conn->query($sql);
 
 // Check if the table is empty
-if ($result->num_rows > 0) {
+if ($result->num_rows > 0 || $result->num_rows == null) {
     // Fetch all data
     $data = array();
     while($row = $result->fetch_assoc()) {
+        foreach ($row as $key => $value) {
+            if (is_null($value)) {
+                continue;
+            }
+
+            $fieldType = $result->fetch_field_direct(array_search($key, array_keys($row)))->type;
+            if ($fieldType == MYSQLI_TYPE_BLOB) {
+                $row[$key] = base64_encode($value);
+            }
+        }
         $data[] = $row;
     }
     echo json_encode($data);
 } else {
-    echo json_encode(array());
+    echo json_encode(["success" => "No data found"]);
 }
 
 // Close connection
 $conn->close();
-?>
