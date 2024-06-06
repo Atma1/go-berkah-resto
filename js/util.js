ec589bfc9bdb4926ae373ad37b14531ea2741ef1
Atma1 <atmasusanto08@gmail.com>
2024-06-06 13:33:15 +0800

    added getProduct function

--- js/util.js
+++ js/util.js
@@ -1,3 +1,21 @@
-export const foo = () => console.log("bar");
 
-export default foo;
+
+export const getProduct = async (route) => {
+    try {
+        const validRoutes = ['makanan', 'sidedish', 'minuman'];
+        if (!validRoutes.includes(route)) {
+            throw new Error('Invalid route');
+        }
+
+        const response = await fetch(`../admin_dashboard/model/model.php?route=${route}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+export default getProduct;
