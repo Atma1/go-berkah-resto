2dceeac28f8d52b648a92464a756fe9a363bf934
Atma1 <atmasusanto08@gmail.com>
2024-06-14 15:08:47 +0800

    made the api more modular

--- admin_dashboard/model/model.php
+++ admin_dashboard/model/model.php
@@ -21,39 +21,48 @@ if ($conn->connect_error) {
 
 // Get the route from the URL
 $route = isset($_GET['route']) ? $_GET['route'] : '';
+$productRoute = isset($_GET['product']) ? $_GET['product'] : '';
 
 // Validate the route
-$validRoutes = array('makanan', 'sidedish', 'minuman');
-if (!in_array($route, $validRoutes)) {
+$product = array('makanan', 'sidedish', 'minuman');
+$crudRoute = array('add', 'delete', 'update', 'get');
+
+if (!in_array($route, $crudRoute)) {
     echo json_encode(array("error" => "Invalid route"));
+    $conn->close();
+    exit();
+} elseif (!in_array($productRoute, $product)) {
+    echo json_encode(array("error" => "No product specified!"));
     $conn->close();
     exit();
 }
 
-// Fetch data from the corresponding table
-$sql = "SELECT id, nama, harga, img, keterangan FROM $route";
-$result = $conn->query($sql);
+if ($route == "get") {
+    $sql = "SELECT id, nama, harga, img, keterangan FROM $productRoute";
+    $result = $conn->query($sql);
 
-// Check if the table is empty
-if ($result->num_rows > 0 || $result->num_rows == null) {
-    // Fetch all data
-    $data = array();
-    while($row = $result->fetch_assoc()) {
-        foreach ($row as $key => $value) {
-            if (is_null($value)) {
-                continue;
-            }
+    // Check if the table is empty
+    if ($result->num_rows > 0 || $result->num_rows == null) {
+        // Fetch all data
+        $data = array();
+        while($row = $result->fetch_assoc()) {
+            foreach ($row as $key => $value) {
+                if (is_null($value)) {
+                    continue;
+                }
 
-            $fieldType = $result->fetch_field_direct(array_search($key, array_keys($row)))->type;
-            if ($fieldType == MYSQLI_TYPE_BLOB) {
-                $row[$key] = base64_encode($value);
+                $fieldType = $result->fetch_field_direct(array_search($key, array_keys($row)))->type;
+                if ($fieldType == MYSQLI_TYPE_BLOB) {
+                    $row[$key] = base64_encode($value);
+                }
             }
+            $data[] = $row;
         }
-        $data[] = $row;
+        echo json_encode(["status" => "success", "data"=>$data]);
+    } else {
+        echo json_encode(["status" => "success", "data"=>"No data found"]);
     }
-    echo json_encode(["status" => "success", "data"=>$data]);
-} else {
-    echo json_encode(["status" => "success", "data"=>"No data found"]);
+
 }
 
 // Close connection
