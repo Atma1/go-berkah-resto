106026bc8fdb9da6e5becf1215a8eddfbd6ba775
Atma1 <atmasusanto08@gmail.com>
2024-06-18 22:58:14 +0800

    edit menu feature

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import {getProduct, deleteProduct} from './util.js';
+import {getAllProduct, deleteProduct, getProduct} from './util.js'
 
 const select = document.getElementById("jenis-produk");
 const addNewProductButton = document.getElementById("add-product-button");
@@ -27,43 +27,92 @@ const table = new gridjs.Grid({
                 formatter: (_, row) => {
                     return [gridjs.h('button', {
                       className: 'color: red, edit-button',
-                      onClick: () => alert(`Editing "${row.cells[0].data}" "${row.cells[1].data}"`)
+                      onClick: () => onEditProductClick(row.cells)
                     }, 'Edit'),
                     gridjs.h('button', {
                         className: 'color: red delete-button',
                         onClick: () => onDeleteProductClick(row.cells[0].data)
                       }, 'Hapus')];
                   }
-            }],
+            }, 
+            {
+                name: "test",
+                hidden: true
+            }
+    ],
     data: []
-    }
-).render(document.getElementById("wrapper"));
+}).render(document.getElementById("wrapper"));
 
 const onSelectChange = async () => {
-    const select = document.getElementById("jenis-produk");
     const selected = select.options[select.selectedIndex].value;
-    const {message, data} = await getProduct("get", selected);
-    if (message == "No data found") return;
+    const {message, data} = await getAllProduct(selected);
+    if (message === "No data found") return;
     dataArray = data.map((dataObject) => Object.values(dataObject));
     table.updateConfig({data: dataArray}).forceRender();
 }
 
-const onAddButtonClick = () => {
-    const select = document.getElementById("jenis-produk");
+const onEditProductClick = async (row) => {
+    // Fetch product data
     const selected = select.options[select.selectedIndex].value;
+    document.getElementById('update-product-id').value = row[0].data;
+    document.getElementById('modal_used').value = selected;
+    document.getElementById('update-product-name').value = row[1].data;
+    document.getElementById('update-product-price').value = row[2].data
+    document.getElementById('update-image-preview').src = `data:image/png;base64,${row[3].data}`;
+    document.getElementById('update-product-description').value = row[4].data;
+    document.getElementById('update-product-category').value = row[5].data;
+
+    // Show modal
+    const updateModal = new bootstrap.Modal(document.getElementById('updateProductModal'));
+    updateModal.show();
+
+    if (selected != "makanan") {
+        document.getElementById('update-makanan-category').style.visibility = 'hidden';
+    }
 }
 
 const onDeleteProductClick = async (productId) => {
-    const select = document.getElementById("jenis-produk");
     const selected = select.options[select.selectedIndex].value;
-    const result = await deleteProduct("delete", selected, productId);
-    if (result) {
-        const updatedData = dataArray.filter((data) => data[0] != productId);
-        dataArray = updatedData;
-        table.updateConfig({data: dataArray}).forceRender();
+    const alertDialog = await Swal.fire({
+        title: "Yakin akan menghapus produk ini?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#CB997E",
+        cancelButtonColor: "#A5A58D",
+        confirmButtonText: "Hapus",
+        background: "#FFE8D6"
+    })
+    if (alertDialog.isConfirmed) {
+        const result = await deleteProduct(selected, productId);
+        if (result) {
+            const updatedData = dataArray.filter((data) => data[0] != productId);
+            dataArray = updatedData;
+            table.updateConfig({data: dataArray}).forceRender();
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil!',
+                text: 'Produk berhasil dihapus!',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }   
     }
 }
 
 select.addEventListener('change', onSelectChange);
-addNewProductButton.addEventListener("click", onAddButtonClick);
-  
+
+document.getElementById('update-product-price-image').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const imagePreview = document.getElementById('update-image-preview');
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = 'block';
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
--- js/util.js
+++ js/util.js
@@ -1,13 +1,13 @@
 
 
-export const getProduct = async (route, productRoute) => {
+export const getAllProduct = async (productRoute) => {
     try {
         const validRoutes = ['makanan', 'sidedish', 'minuman'];;
         if (!validRoutes.includes(productRoute)) {
             throw new Error('Invalid route');
         }
 
-        const response = await fetch(`../admin_dashboard/model/model.php?route=${route}&product=${productRoute}`);
+        const response = await fetch(`../admin/model/model.php?route=get&product=${productRoute}`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -18,10 +18,30 @@ export const getProduct = async (route, productRoute) => {
     }
 }
 
-export const deleteProduct = async (route, productRoute, productId) => {
+export const getProduct = async (productRoute, productId) => {
+    try {
+        const validRoutes = ['makanan', 'sidedish', 'minuman'];;
+        if (!validRoutes.includes(productRoute)) {
+            throw new Error('Invalid route');
+        }
+
+        const response = await
+        fetch(`../admin/model/model.php?route=get&product=${productRoute}&productId=${productId}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+export const deleteProduct = async (productRoute, productId) => {
     try {
         const response = await
-        fetch(`../admin_dashboard/model/model.php?route=${route}&product=${productRoute}&productId=${productId}`);
+        fetch(`../admin/model/model.php?route=delete&product=${productRoute}&productId=${productId}`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -34,4 +54,4 @@ export const deleteProduct = async (route, productRoute, productId) => {
     }
 }
 
-export default {getProduct, deleteProduct};
+export default {getAllProduct, deleteProduct, getAllProduct};
